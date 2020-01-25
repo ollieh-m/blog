3d9d12d8b584348205bb804e7be6ebedf1241c1c
Ollie Haydon-Mulligan <ollie.bhm@gmail.com>
2020-01-25 18:27:58 +0000

    Reorder methods

--- lib/commands/articles_command.rb
+++ lib/commands/articles_command.rb
@@ -13,22 +13,7 @@ class ArticlesCommand < Rails::Command::Base
   end
 
   no_commands do
-    def articles_not_in_database
-      articles_from_config.reverse.take_while do |article_attributes|
-        article_attributes["view_id"] != latest_persisted_view_id
-      end
-    end
-
-    def articles_from_config
-      @_articles_from_config ||= YAML.load(File.open(Rails.root.join("config", "articles.yml"))) || []
-    end
-
-    def latest_persisted_view_id
-      @_latest_persisted_view_id ||= Article.recent_first.first&.view_id
-    end
-
     def articles_not_in_config
-      byebug
       Article.where.not(view_id: view_ids_from_config)
     end
 
@@ -36,6 +21,20 @@ class ArticlesCommand < Rails::Command::Base
       articles_from_config.map do |article_attributes|
         article_attributes["view_id"]
       end
+    end
+
+    def articles_from_config
+      @_articles_from_config ||= YAML.load(File.open(Rails.root.join("config", "articles.yml"))) || []
+    end
+
+    def articles_not_in_database
+      articles_from_config.reverse.take_while do |article_attributes|
+        article_attributes["view_id"] != latest_persisted_view_id
+      end
+    end
+
+    def latest_persisted_view_id
+      @_latest_persisted_view_id ||= Article.recent_first.first&.view_id
     end
   end
 end
