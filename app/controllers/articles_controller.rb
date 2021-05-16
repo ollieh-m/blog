54a6054b7358c8c2b98a295a6aa54c7f66635251
Ollie Haydon-Mulligan <ollie.bhm@gmail.com>
2021-05-16 15:36:43 +0100

    Refactor grid and add placeholders to fill empty space

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,37 @@
 class ArticlesController < ApplicationController
+  ROW_SIZE = 4
+
   def show 
     @article = Article.find(params[:id])
   end
 
   def index
-    @articles = Article.recent_first
+    @articles = articles + top_ups
   end
+
+  private
+
+    def articles
+      @_articles ||= fetch_articles
+    end
+
+    def fetch_articles
+      Article.recent_first
+    end
+
+    def top_ups
+      [ArticleComingSoon.new] * empty_row_positions
+    end
+
+    def empty_row_positions
+      if row_positions_filled > 0
+        ROW_SIZE - row_positions_filled
+      else
+        0
+      end
+    end
+
+    def row_positions_filled
+      articles.length % ROW_SIZE
+    end
 end
---
+++ app/models/article_coming_soon.rb
@@ -0,0 +1,13 @@
+class ArticleComingSoon
+  def title
+    "Coming soon"
+  end
+
+  def created_at
+    "some time in the future"
+  end
+
+  def colour
+    "gray"
+  end
+end
