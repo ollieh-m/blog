56583d1411c2884b86ac90131d463c4038044c55
Ollie Haydon-Mulligan <ollie.bhm@gmail.com>
2021-06-27 22:07:09 +0100

    Add favicon

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
   def icon(name, **options)
-    inline_svg_pack_tag "media/icons/#{name}.svg", options
+    inline_svg_pack_tag "media/images/#{name}.svg", options
   end
 end
